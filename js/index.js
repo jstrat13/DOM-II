f33e34c87fb9239abdd3aba2cc247e139e3a6f84
jstrat13 <49758720+jstrat13@users.noreply.github.com>
2019-06-09 20:51:43 -0500

    added event listeners to h1 and btns

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
-const nav = document.querySelectorAll('a');
-console.log(nav);
+const nav = document.querySelectorAll('a');                          // nav events
+
 
 nav.forEach(item => item.addEventListener('mouseover', (e) => {
     item.style.fontSize = '3rem';
@@ -8,4 +8,30 @@ nav.forEach(item => item.addEventListener('mouseover', (e) => {
 
 nav.forEach(item => item.addEventListener('mouseout', (e) => {
     item.style.fontSize = '1.6rem';
+}));                                                                  // nav events
+
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('dblclick', (e) => {
+    h1.style.color = 'dodgerblue';
+});
+
+const btn  = document.querySelectorAll('.btn');
+
+btn.forEach(item => item.addEventListener('mousedown', (e) => {
+    item.style.backgroundColor = 'chartreuse';
 }));
+
+btn.forEach(item => item.addEventListener('mouseover', (e) => {
+    item.style.fontColor = 'white';
+}));
+
+btn.forEach(item => item.addEventListener('mouseup', (e) => {
+    item.style.backgroundColor = '#17A2B8';
+}));
+
+
+
+
+
+console.log(btn);
