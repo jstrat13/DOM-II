4c3d37ed47632b3f9282eab6c2702504c76119d4
jstrat13 <49758720+jstrat13@users.noreply.github.com>
2019-06-09 22:07:32 -0500

    added event listeners for img and demonstrated stop prop

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+
 const nav = document.querySelectorAll('a');                          // nav events
 
 
@@ -16,22 +18,31 @@ h1.addEventListener('dblclick', (e) => {
     h1.style.color = 'dodgerblue';
 });
 
-const btn  = document.querySelectorAll('.btn');
+const btn  = document.querySelectorAll('.btn');                      // btn events
 
-btn.forEach(item => item.addEventListener('mousedown', (e) => {
+btn.forEach((item) => item.addEventListener('mousedown', (e) => {
     item.style.backgroundColor = 'chartreuse';
 }));
 
-btn.forEach(item => item.addEventListener('mouseover', (e) => {
-    item.style.fontColor = 'white';
-}));
-
-btn.forEach(item => item.addEventListener('mouseup', (e) => {
+btn.forEach((item) => item.addEventListener('mouseup', (e) => {
     item.style.backgroundColor = '#17A2B8';
+}));                                                                  // btn events
+
+const pics = document.querySelectorAll('img');
+// pics[0].addEventListener('click', () => {
+//     pics[0].style.display = 'none';
+// });
+
+pics.forEach((item) => item.addEventListener('click', (e) => {
+    e.stopPropagation();
+    item.style.display = 'none';
 }));
 
+const content = document.querySelector('.content-section');
+content.addEventListener('click', () => {
+    content.style.backgroundColor = 'red';
+});
+console.log(content)
 
 
 
-
-console.log(btn);
