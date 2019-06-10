61e7f8ba9e23c7cf201f79ba1b775b39d36c4b4c
jstrat13 <49758720+jstrat13@users.noreply.github.com>
2019-06-09 20:06:25 -0500

    added event listeners to nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const nav = document.querySelectorAll('a');
+console.log(nav);
+
+nav.forEach(item => item.addEventListener('mouseover', (e) => {
+    item.style.fontSize = '3rem';
+}));
+
+nav.forEach(item => item.addEventListener('mouseout', (e) => {
+    item.style.fontSize = '1.6rem';
+}));
